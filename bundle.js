782344255e35ce063a8a789eb800836ed495d1c5
sujayvenaik <sujayvenaik@gmail.com>
2018-03-05 18:50:22 +0530

    213

--- bundle.js
+++ bundle.js
@@ -1,9 +1,10 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.xyz = f()}})(function(){var define,module,exports;return (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
 // Exports the interface for the plugin
-module.exports = {
+Converter = {
 	validate: require('./lib/validate'),
 	convert: require('./lib/convert')
 };
+
 },{"./lib/convert":2,"./lib/validate":4}],2:[function(require,module,exports){
 var Collection = require('postman-collection').Collection,
   _ = require('lodash'),
