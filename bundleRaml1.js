6339fa59b027b4d11effc5929bbb706f266bfa56
sujayvenaik <sujayvenaik@gmail.com>
2018-03-07 01:34:25 +0530

    Async add

--- bundleRaml1.js
+++ bundleRaml1.js
@@ -3,6 +3,21 @@ var async = require('async');
 
 Converter = {
     convert: function(){
+        async.series({
+            one: function(callback) {
+                setTimeout(function() {
+                    callback(null, 1);
+                }, 200);
+            },
+            two: function(callback){
+                setTimeout(function() {
+                    callback(null, 2);
+                }, 100);
+            }
+        }, function(err, results) {
+            console.log(results);
+            // results is now equal to: {one: 1, two: 2}
+        });
         console.log('Ho');
     }
 }
