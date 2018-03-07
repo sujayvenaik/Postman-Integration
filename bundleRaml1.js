44c33e8f1571354d4fef1d9e789942fe90cf1c29
sujayvenaik <sujayvenaik@gmail.com>
2018-03-07 10:10:41 +0530

    Maalpoi

--- bundleRaml1.js
+++ bundleRaml1.js
@@ -1,31 +1,16 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.xyz = f()}})(function(){var define,module,exports;return (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
-var async = require('async'),
-    jsonschema = require('json-schema-faker'),
-    _ = require('lodash'),
-    oauth = require('oauth-signature'),
-    path = require('path'),
-    sdk = require('postman-collection'),
-    validator = require('postman_validator'),
-    raml = require('raml-parser');
+var async = require('async');
+var    jsonschema = require('json-schema-faker');
+var _ = require('lodash');
+var oauth = require('oauth-signature');
+var path = require('path');
+var sdk = require('postman-collection');
+var validator = require('postman_validator');
+var raml = require('raml-parser');
 
 Converter = {
     convert: function(){
-        async.series({
-            one: function(callback) {
-                setTimeout(function() {
-                    callback(null, 1);
-                }, 200);
-            },
-            two: function(callback){
-                setTimeout(function() {
-                    callback(null, 2);
-                }, 100);
-            }
-        }, function(err, results) {
-            console.log(results);
-            // results is now equal to: {one: 1, two: 2}
-            console.log('Finish');
-        });
+        console.log('Ho');
     }
 }
 },{"async":10,"json-schema-faker":169,"lodash":198,"oauth-signature":206,"path":303,"postman-collection":210,"postman_validator":250,"raml-parser":261}],2:[function(require,module,exports){
